2e4eb6db00d91e21bbe7bc9b43b96ae266ec0f20
Victoria Hale <vic.hale74@gmail.com>
2019-11-20 20:24:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,10 +2,10 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  # configure do 
-  #   enable :sessions
-  #   set :sesion_secret, "secret"
-  # end
+  configure do 
+    enable :sessions
+    set :sesion_secret, "secret"
+  end
   
   # get '/' do 
   #   erb :index
