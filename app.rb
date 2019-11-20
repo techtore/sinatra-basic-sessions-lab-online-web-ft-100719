1cf33f34ad78d15eb41ccdd44856d6dc60b399da
Victoria Hale <vic.hale74@gmail.com>
2019-11-20 20:22:45 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,18 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
-  configure do 
-    enable :sessions
-    set :sesion_secret, "secret"
-  end
+  # configure do 
+  #   enable :sessions
+  #   set :sesion_secret, "secret"
+  # end
   
-  get '/' do 
-    erb :index
-  end
+  # get '/' do 
+  #   erb :index
+  # end
   
-  post '/checkout'
-    @session = session
-    item = params["item"]
-    @session[:item] = item
-  end
+  # post '/checkout'
+  #   @session = session
+  #   item = params["item"]
+  #   @session[:item] = item
+  # end
 end
